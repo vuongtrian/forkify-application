a0040eba101df61331855bc98aa7c4073fe20556
vuongtrian <vuongtrian116@gmail.com>
2021-08-06 22:13:57 +0700

    Initial commnit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8f5c5bb6-fb14-4e1c-9197-125da3449e3b';
+export const MODAL_CLOSE_SEC = 2.5;
